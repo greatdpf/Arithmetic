20bc7386f752bc492d20d67af03873019513283d
dpf <greatdpf@163.com>
2020-10-21 16:28:26 +0800

    选择排序算法

--- ChangeSort/src/com/greatdpf/ChangeSort.java
+++ ChangeSort/src/com/greatdpf/ChangeSort.java
@@ -1,11 +1,18 @@
 package com.greatdpf;
 
 /**
- * Author: staring
- * Date: 2020/10/21
- * Version: V1.0
- * Describe: 选择排序
+ * @author : staring
+ * @date : 2020/10/21
+ * @version : V1.0
+ * @description : 选择排序
  */
 public class ChangeSort {
-
+    /**
+     * @description : 程序的入口
+     * @author : staring
+     * @date : 2020/10/21
+     */
+    public static void main(String[] args) {
+        System.out.println("选择排序算法");
+    }
 }
